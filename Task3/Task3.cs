c287291167d054869cac6d8d9c452092bf6775c7
Lvmba <32847640+lvmba@users.noreply.github.com>
2026-03-31 14:15:16 +0400

    Update Task3.cs

--- Task3/Task3.cs
+++ Task3/Task3.cs
@@ -1,12 +1,45 @@
-﻿using System.Text.Json;
+using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.IO;
 
-Console.Write("Путь к values.json: ");
-string inputValues = Console.ReadLine();
-Console.Write("Путь к tests.json: ");
-string inputTest = Console.ReadLine();
-Console.Write("Путь к отчету: ");
-string output = Console.ReadLine();
+if (args.Length < 3)
+{
+    Console.WriteLine("Использование: dotnet run <путь_к_values.json> <путь_к_tests.json> <путь_к_отчету.json>");
+    return;
+}
+
+string inputValues = args[0];
+string inputTest = args[1];
+string output = args[2];
+
+if (!File.Exists(inputValues))
+{
+    Console.WriteLine($"Файл не найден: {inputValues}");
+    return;
+}
+
+if (!File.Exists(inputTest))
+{
+    Console.WriteLine($"Файл не найден: {inputTest}");
+    return;
+}
+
+var node1 = JsonNode.Parse(File.ReadAllText(inputValues));
+var node2 = JsonNode.Parse(File.ReadAllText(inputTest));
+
+bool node1HasValues = node1["values"] != null;
+bool node2HasTests = node2["tests"] != null;
+if (!node1HasValues)
+{
+    Console.WriteLine("Первый файл не содержит структуру Values");
+    return;
+}
+if (!node2HasTests)
+{
+    Console.WriteLine("Второй файл не содержит структуру Tests");
+    return;
+}
+
 File.Copy(inputTest, output, overwrite: true);
 
 var values = JsonNode.Parse(File.ReadAllText(inputValues));
