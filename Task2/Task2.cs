e0a6f1eb13e622998ac68fdc841e431417c53669
Lvmba <32847640+lvmba@users.noreply.github.com>
2026-03-31 14:14:44 +0400

    Update Task2.cs

--- Task2/Task2.cs
+++ Task2/Task2.cs
@@ -1,27 +1,64 @@
-﻿Console.Write("Путь к файлу с координатами:");
-string input1 = Console.ReadLine();
-Console.Write("Путь к файлу с точками:");
-string input2 = Console.ReadLine();
+using System;
+using System.Collections.Generic;
+using System.IO;
+if (args.Length < 2)
+{
+    Console.WriteLine("Использование: dotnet run <путь_к_файлу_эллипса> <путь_к_файлу_точек>");
+return;
+}
+
+string input1 = args[0];
+string input2 = args[1];
+
+if (!File.Exists(input1))
+{
+    Console.WriteLine("Файл с параметрами эллипса не найден");
+    return;
+}
+
+if (!File.Exists(input2))
+{
+    Console.WriteLine("Файл с параметрами эллипса не найден");
+    return;
+}
+
 List<int> ans = [];
+const decimal Epsilon = 0.0000000001m;
 
 string[] ellipseParam = File.ReadAllLines(input1);
-var centerCoords = ellipseParam[0].Split(' ');
-double x0 = double.Parse(centerCoords[0]);
-double y0 = double.Parse(centerCoords[1]);
-var radiusXY = ellipseParam[1].Split(' ');
-double a = double.Parse(radiusXY[0]);
-double b = double.Parse(radiusXY[1]);
+var centerCoords = ellipseParam[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+decimal x0 = decimal.Parse(centerCoords[0]);
+decimal y0 = decimal.Parse(centerCoords[1]);
+var radiusXY = ellipseParam[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+decimal a = decimal.Parse(radiusXY[0]);
+decimal b = decimal.Parse(radiusXY[1]);
+
+if (a == 0 || b == 0)
+{
+    Console.WriteLine("Ошибка: Радиусы не могут быть равны нулю");
+    return;
+}
+
+if (a < 0)
+{
+    a = Math.Abs(a);
+}
+
+if (b < 0)
+{
+    b = Math.Abs(b);
+}
 
 string[] coords = File.ReadAllLines(input2);
 for (int i = 0; i < coords.Length; i++)
 {
-    var coordXY = coords[i].Split(' ');
-    double x = double.Parse(coordXY[0]);
-    double y = double.Parse(coordXY[1]);
-    double ellipseCanonic = ((x - x0) * (x - x0) / (a * a)) + ((y - y0) * (y - y0) / (b * b));
-    if (ellipseCanonic == 1)
+    var coordXY = coords[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    decimal x = decimal.Parse(coordXY[0]);
+    decimal y = decimal.Parse(coordXY[1]);
+    decimal ellipseCanonic = ((x - x0) * (x - x0) / (a * a)) + ((y - y0) * (y - y0) / (b * b));
+    if (Math.Abs(ellipseCanonic - 1m) <= Epsilon)
         ans.Add(0);
-    else if (ellipseCanonic < 1)
+    else if (ellipseCanonic < 1m)
         ans.Add(1);
     else
         ans.Add(2);
